11ee371db482daa842aee6c46c34e5585dcb3deb
olsonperrensen <r0834721@student.thomasmore.be>
2024-04-10 10:25:59 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics /></body>
     </html>
   );
 }
